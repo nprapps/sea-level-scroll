f993e75597a8985a88f2a673e9b2326de08c20a9
ruthwtalbot <59851648+ruthwtalbot@users.noreply.github.com>
2021-06-21 17:01:20 -0400

    More map styles and map tweaks

--- src/js/mapView.js
+++ src/js/mapView.js
@@ -6,35 +6,45 @@ var labelKey = require("../../data/label_keys.sheet.json");
 var mapElement = $.one("#base-map");
 
 var active;
+var mapAssets = {};
 
 var enter = function (slide, map) {
   // if (slide == active) return;
   mapElement.classList.remove("hidden");
+  var currLayer = mapKey[slide.id];
+  var assets = currLayer.assets
+    ? currLayer.assets.split(",").map(d => d.trim())
+    : [];
 
+  // Remove old layers.
   map.eachLayer(function (layer) {
     if (layer.options.id == "baseLayer") return;
     map.removeLayer(layer);
   });
-  var currLayer = mapKey[slide.id];
 
-  if (currLayer.assets) {
-    currLayer.assets.split(",").forEach(function (a) {
-      var styles = { className: assetKey[a].classNames };
-      fetchAsset(assetKey[a].path).then(d =>
-        L.geoJSON(d, { style: styles }).addTo(map)
-      );
-    });
-  }
+  // Add new layers onto slide.
+  assets.forEach(function (a) {
+    if (mapAssets[a]) {
+      mapAssets[a].addTo(map);
+    } else {
+      var styles = { className: assetKey[a].classNames.split(",").join("") };
+      fetchAsset(assetKey[a].path).then(function (d) {
+        mapAssets[a] = L.geoJSON(d, { id: a, style: styles });
+        mapAssets[a].addTo(map);
+      });
+    }
+  });
+
   if (currLayer.label_ids) {
     currLayer.label_ids.split(",").forEach(function (a) {
       var label = labelKey[a];
       if (!label) return;
-      var [lat, lon] = label.lat_long.split(',');
+      var [lat, lon] = label.lat_long.split(",");
       new L.Marker([lat, lon], {
         icon: new L.DivIcon({
-          className: label.classNames.split(",").join(' '),
-          html:
-            `<span>${label.label}</span>`,
+          className: label.classNames.split(",").join(" "),
+          html: `<span>${label.label}</span>`,
+          iconSize: [150, 30],
         }),
       }).addTo(map);
     });
@@ -48,9 +58,10 @@ var enter = function (slide, map) {
     bounds = L.latLngBounds(southWest, northEast);
 
   if (currLayer.duration > 0) {
-    map.flyToBounds(bounds, currLayer.zoomLevel, {
+    map.flyToBounds(bounds, {
       animate: true,
-      duration: currLayer.duration,
+      duration: 2,
+      easeLinearity: 1,
     });
   } else {
     map.fitBounds(bounds, currLayer.zoomLevel);
@@ -61,7 +72,6 @@ var enter = function (slide, map) {
 };
 
 function style(feature) {
-  console.log(feature);
   return {
     weight: 3,
     color: "#fff",
@@ -93,5 +103,16 @@ var fetchAsset = async function (asset) {
   var json = await response.json();
   return json;
 };
+
+var loadAssets = (function () {
+  Object.entries(assetKey).forEach(function (a) {
+    var [id, value] = a;
+    if (!value.path) return;
+    var styles = { className: value.classNames.split(",").join("") };
+    fetchAsset(value.path).then(
+      d => (mapAssets[id] = L.geoJSON(d, { id: id, style: styles }))
+    );
+  });
+})();
 
 module.exports = { enter, exit, preload };
