23c8c5af0b22ecd15064e70f03f8ed7dd05d865e
ruthwtalbot <59851648+ruthwtalbot@users.noreply.github.com>
2021-07-21 15:51:56 -0400

    Add poster tag to videos

--- src/js/imageView.js
+++ src/js/imageView.js
@@ -36,5 +36,10 @@ module.exports = class ImageView extends View {
       img.src = img.dataset.src;
       img.removeAttribute("data-src");
     });
+    var posters = $("[data-poster]", slide);
+    images.forEach(function (img) {
+      img.poster = img.dataset.poster;
+      img.removeAttribute("data-poster");
+    });
   }
 };
