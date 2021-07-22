cf83bbf5a134b740f030f3df3bac84d2aa0890b8
ruthwtalbot <59851648+ruthwtalbot@users.noreply.github.com>
2021-07-22 11:52:55 -0400

    Tweak grid images and zoomSnap

--- src/js/main.js
+++ src/js/main.js
@@ -12,7 +12,7 @@ var slides = $(".sequence .slide").reverse();
 var autoplayWrapper = $.one(".a11y-controls");
 
 // Initialize leaflet map
-var map = L.map("base-map", { zoomControl: false, fadeAnimation: false, markerZoomAnimation: false}).setView(
+var map = L.map("base-map", { zoomSnap: .5, zoomControl: false, fadeAnimation: false, markerZoomAnimation: false}).setView(
   [37.466623667154515, -122.06826378148338],
   13
 );
